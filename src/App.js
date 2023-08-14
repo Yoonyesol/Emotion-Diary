1640e2ee526196b2471bb1b1d4dc5e2856bc7611
yesol <maniac970430@naver.com>
2023-08-14 20:55:23 +0900

    페이지 생성 및 페이지 라우팅(React Router 기본)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter> {/*감싼 부분은 브라우저의 url과 매핑 가능*/}
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes> {/*페이지를 경로에 따라 매핑*/}
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New/>}/>
+          <Route path='/edit' element={<Edit />} />
+          <Route path='/diary' element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
