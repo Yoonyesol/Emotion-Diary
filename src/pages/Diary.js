2a84d4341d13433f0263117df013984adca5b2ac
yesol <maniac970430@naver.com>
2023-08-15 23:22:43 +0900

    페이지 라우팅 2 - React Router 응용(useParams, useSearchParams, useNavigate)

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,10 @@
+import { useParams } from "react-router";
+
 const Diary = () => {
+  //Path Variable
+  const { id } = useParams(); //useParams:전달받은 path variable들을 객체로 모아 전달해 준다.
+  console.log("id: ", id);
+  
   return <div>
     <h1>Diary</h1>
     <p>이곳은 일기 상세 페이지입니다.</p>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,23 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  //useNavigate: 페이지를 이동시킬 수 있는 함수 반환
+  const navigate = useNavigate(); 
+
+  //Query String 받아서 사용하기
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode")
+  console.log("mode: ", mode)
+
   return <div>
     <h1>Edit</h1>
     <p>이곳은 일기 수정 페이지입니다.</p>
-  </div>
+    <button onClick={() => setSearchParams({who: "coco"})}>QS 바꾸기</button>
+    <button onClick={() => { navigate("/home"); }}>HOME으로 가기</button>
+    <button onClick={()=>{navigate(-1)}}>뒤로 가기</button>
+  </div>;
 };
 export default Edit;
