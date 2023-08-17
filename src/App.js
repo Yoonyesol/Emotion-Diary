fc231ab3e885c680aa7002f5df392d645bc45c83
yesol <maniac970430@naver.com>
2023-08-17 20:48:44 +0900

    프로젝트 기초 공사 2(CRUD 기능 작성 / reducer, context 이용해 모든 컴포넌트에 일기 데이터를 내려주기)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,89 @@
+import React, { useReducer, useRef } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import MyButton from './components/MyButton';
-import MyHeader from './components/MyHeader';
+
+const reducer = (state, action) => {
+  let newState = []
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it);
+        break;
+    };
+    default:
+      return state;
+  }
+  return newState;
+}
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
+  //data: 일기데이터를 관리하는 상태변수
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE", data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content, emotion,
+      }
+    });
+    dataId.current += 1;
+  }
+
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type: "REMOVE", targetId})
+  }
+
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT", data: {
+        id: targetId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+    }})
+  }
+
   return (
-    <BrowserRouter> {/*감싼 부분은 브라우저의 url과 매핑 가능*/}
-      <div className="App">
-        <MyHeader headText={"App"}
-          leftChild={<MyButton text={'왼쪽 버튼'} onClick={() => alert("왼쪽 클릭")} />}
-          rightChild={<MyButton text={'오른쪽 버튼'} onClick={() => alert("오른쪽 클릭")} />}
-        />
-        <h2>App.js</h2>
-        <MyButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={"positive"} />
-        <MyButton text={'버튼'} onClick={()=>alert("버튼 클릭")} type={"negative"} />
-        <MyButton text={'버튼'} onClick={()=>alert("버튼 클릭")}/>
-        <Routes> {/*페이지를 경로에 따라 매핑*/}
-          <Route path='/' element={<Home />} />
-          <Route path='/new' element={<New/>}/>
-          <Route path='/edit' element={<Edit />} />
-          <Route path='/diary/:id' element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{onCreate, onEdit, onRemove}}>
+        <BrowserRouter> {/*감싼 부분은 브라우저의 url과 매핑 가능*/}
+          <div className="App">
+            <Routes> {/*페이지를 경로에 따라 매핑*/}
+              <Route path='/' element={<Home />} />
+              <Route path='/new' element={<New/>}/>
+              <Route path='/edit' element={<Edit />} />
+              <Route path='/diary/:id' element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+    
   );
 }
 
